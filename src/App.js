0b26fc8cebf0532d1faebba213eced71991daf4f
Christopher-Deo <chris@christopherdeo.com>
2023-02-15 22:22:43 -0600

    dependencies installed and file structure set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardList from './components/cardList/CardList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+    console.log('constructor');
+  }
+
+  //componentDidMount()method is used to get api data when the component is first loaded into the DOM. Only 1x/component lifecycle
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      //fetch returns a promise
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+        return { monsters: users };
+      },
+        () => {
+          console.log(this.state);
+        }
+      ));
+  }
+
+  handleChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    }
+  )}
+
+
+  render() {
+    console.log('render');
+//using destructuring to improve performance
+    const { monsters, searchField } = this.state;
+    const { handleChange } = this;
+
+
+    // const filteredMonsters = monsters.filter((monster) => {
+    //   //callback is expecting a boolean
+    //   //.includes returns true when conditions are met
+    //   return monster.name.toLocaleLowerCase().includes(searchField);
+    // });
+
+    return (
+      <div className='App d-flex flex-column align-items-center '>
+        <input
+          className="search=box mt-5"
+          type='search'
+          placeholder="search monsters"
+          onChange={ handleChange }
+        />
+        {/* {filteredMonsters.map((monster) => {
+          return <div 
+                    className="p-3" 
+                    key={monster.id} 
+                    >
+                    {monster.name}
+                    </div>;
+        })} */}
+        <CardList monsters={filteredMonsters} />
+      </div>
+
+    );
+  }
+
+};
 
 export default App;
+
+
+
+
+
---
+++ src/components/cardList/CardList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class CardList extends Component{
+
+
+    render() {
+        return <div>Card List Component</div>
+    }
+}
+
+export default CardList;
